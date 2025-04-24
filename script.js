1930511ce35273f65f696b1d116a02a4c47efe85
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-04-24 11:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+let age=document.querySelector("#age");
+let name=document.querySelector("#name");
+let btn=document.querySelector("#btn");
+
+btn.addEventListener("click",(e)=>{
+	
+	if(age.value.trim().length<=0 || name.value.trim().length<=0){
+		alert("Please enter valid details.");
+		return;
+	}
+	let username=name.value.trim();
+	let userage=parseInt(age.value.trim());
+
+	let promise1=new Promise((resolve,reject)=>{
+	if(userage >=18){
+	setTimeout(()=>{
+		resolve(`Welcome, ${username}. You can vote`);
+	},4000);
+	}
+	else{
+		setTimeout(()=>{
+		reject(`Oh sorry ${username}. You aren't old enough.`);
+	},4000);
+	}
+	
+});
+	promise1
+		.then((msg)=>alert(msg))
+		.catch((err) => alert(err));
+});
+
